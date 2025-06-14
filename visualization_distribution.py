eaa3d1b43e81a33214f6970007c4195542f17885
Marin, Daniel <m.marin@student.tue.nl>
2025-06-14 12:01:53 +0200

    added sentiment change distribution visualisation

--- visualization_distribution.py
+++ visualization_distribution.py
@@ -7,7 +7,7 @@ import os
 NEO4J_URI = "bolt://localhost:7687"
 NEO4J_USER = "neo4j"
 NEO4J_PASSWORD = "password"
-NEO4J_DB = "final"
+NEO4J_DB = "database1"
 AMERICAN_AIR_ID = 22536055
 EXPORT_DIR = "export"
 
